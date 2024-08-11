77e51092e882fc564b00697d23245c897ff71753
Emmanuel <emmanuelugochukwu2000@gmail.com>
2024-08-11 17:49:46 +0100

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
-
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession()
   return (
    <div className="bg-blue-900 w-screen h-screen flex items-center">
     <div className="text-center w-full">
